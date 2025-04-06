5aef86f17d3381c7d98c058727f2fecade098366
Karthik K <98963797+sai-karthik-k@users.noreply.github.com>
2025-04-06 16:14:04 +0530

    Update ke.py

--- ke.py
+++ ke.py
@@ -1,6 +1,7 @@
-import gdown  # add 'gdown' to requirements.txt
+import gdown  # Add 'gdown' to requirements.txt
 
-# Download model from Google Drive (shared as public link)
-url = 'https://drive.google.com/file/d/1frIR6crojKV86I_UtjIZmFinCCb2y6s_/view?usp=sharing'
+# Correct Google Drive link for gdown
+url = 'https://drive.google.com/uc?id=1frIR6crojKV86I_UtjIZmFinCCb2y6s_'
 output = 'model.keras'
+
 gdown.download(url, output, quiet=False)
